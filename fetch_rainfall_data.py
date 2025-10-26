46cc0793b3b916cf73e7b03535db0e5304db9750
HanniKanchap <hannikanchap11@gmail.com>
2025-10-26 15:46:58 +0530

    handling API error

--- fetch_rainfall_data.py
+++ fetch_rainfall_data.py
@@ -4,7 +4,7 @@ from io import StringIO
 import streamlit as st
 
 key = st.secrets['DATA_API_KEY']
-def fetch_rainfall_data(api_key="579b464db66ec23bdd000001c082134a1113493a7b97920f84757672"):
+def fetch_rainfall_data(api_key=key):
     url = f"https://api.data.gov.in/resource/8e0bd482-4aba-4d99-9cb9-ff124f6f1c2f?api-key={api_key}&format=csv&limit=5000"
     response = requests.get(url)
     df = pd.read_csv(StringIO(response.text))
